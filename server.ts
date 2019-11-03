b5a398859d5c015c15d4e199eeec76852ba6f5e6
Lundrii <jadenedhlund@gmail.com>
2019-11-03 13:06:53 -0500

    Fixed server.ts and added squat classifier

--- server.ts
+++ server.ts
@@ -10,7 +10,6 @@ const app = express();
 const PORT = 8080;
 
 app.get("/", async (req, res) => {
-<<<<<<< HEAD
   res.setHeader('Content-Type', 'image/png');
   const image = await loadImage('image2.jpg');
   const canvas = createCanvas(image.width, image.height);
@@ -39,7 +38,7 @@ app.get("/", async (req, res) => {
     let leftShin = Math.sqrt(Math.pow(leftAnkleX - leftKneeX, 2) + Math.pow(leftAnkleY - leftKneeY, 2));
     let left = Math.sqrt(Math.pow(leftHipX - leftAnkleX, 2) + Math.pow(leftHipY - leftAnkleY, 2));
     if (Math.acos((Math.pow(leftThigh, 2) + Math.pow(leftShin, 2)
-        - Math.pow(left, 2)) / (2 * leftThigh * leftShin)) < Math.PI/2) {
+        - Math.pow(left, 2)) / (2 * leftThigh * leftShin)) < Math.PI) {
         lSquat = true;
     }
     console.log(Math.acos((Math.pow(leftThigh, 2) + Math.pow(leftShin, 2)
@@ -50,30 +49,4 @@ app.get("/", async (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`);
-=======
-    res.setHeader('Content-Type', 'image/png');
-    const image = await loadImage('image3.jpg');
-    const canvas = createCanvas(image.width, image.height);
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(image, 0, 0);
-
-    const net = await posenet.load();
-    const pose = await net.estimateSinglePose(canvas as unknown as HTMLCanvasElement);
-    const connectedJoints = posenet.getAdjacentKeyPoints(pose.keypoints, 0);
-
-    ctx.strokeStyle = 'red';
-    ctx.lineWidth = 5;
-    connectedJoints.forEach(points => {
-        ctx.moveTo(points[0].position.x, points[0].position.y);
-        ctx.lineTo(points[1].position.x, points[1].position.y);
-        ctx.stroke();
-    });
-
-    canvas.createPNGStream().pipe(res);
-    console.log(pose);
-});
-
-app.listen(PORT, () => {
-    console.log(`server started at http://localhost:${PORT}`);
->>>>>>> 9bf604b7b38f47ea20540fbc84fa4a140a89e73d
 });
